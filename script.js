f91b0632794c30f9ad37d1eaf0a30222e8b21d3e
Abdul Ghani <ghanisheikh26@gmail.com>
2025-05-31 23:58:09 +0530

    Add Loading Spinner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const input = document.querySelector(".input");
 const searchButton = document.querySelector(".searchButton");
-const liveLocationButton = document.querySelector(".getLive");
+const getLocation = document.querySelector(".getLocationButton");
 const city = document.querySelector(".city");
 const localTime = document.querySelector(".localTime");
 const temp = document.querySelector(".temp");
@@ -10,19 +10,32 @@ const error = document.querySelector(".errorMsg");
 const baseUrl = "http://api.weatherapi.com/v1/forecast.json";
 const apiKey = "dd00ed59dcc54731b6e85238252504";
 const weatherIcon = document.querySelector(".weatherIcon");
+const spinner = document.querySelector(".loader");
+const content = document.querySelector(".content");
+
+function showSpinner() {
+    content.style.display = "none";
+    spinner.style.display = "block";
+}
+
+function hideSpinner() {
+    content.style.display = "block";
+    spinner.style.display = "none";
+}
 
 function failedToGet() {
     city.innerText = "";
     localTime.innerText = "";
     temp.innerText = "";
-    error.innerText = "Invalid Input : No Such City ❌";
+    error.innerText = "There is some issue while fetching your location";
 }
 
 function handleEmptyInput() {
-    error.innerText = "❓ Please Enter City Name";
+    error.innerText = "❓Please Enter City Name";
 }
 
 refreshButton.addEventListener("click", () => {
+    hideSpinner();
     error.innerText = "";
     input.value = "";
     city.innerText = "City";
@@ -31,10 +44,12 @@ refreshButton.addEventListener("click", () => {
     weatherIcon.style.display= "none";
 });
 
-searchButton.addEventListener("click", async () => {
+searchButton.addEventListener('click', async () => {
+    showSpinner();
     const cityName = input.value;
     if(cityName === "") {
         handleEmptyInput();
+        setTimeout(hideSpinner, 500);
         return;
     }
     try {
@@ -44,7 +59,7 @@ searchButton.addEventListener("click", async () => {
         city.innerText = `${result.location.name}, ${result.location.country}`;
         localTime.innerText = result.location.localtime;
         temp.innerText = `${result.current.temp_c}°C`;
-        let isDay = result.current.is_day;
+        const isDay = result.current.is_day;
         if(isDay == 1) {
             weatherIcon.src = `${result.forecast.forecastday[0].day.condition.icon}`;
         }
@@ -55,6 +70,9 @@ searchButton.addEventListener("click", async () => {
     catch(e) {
         failedToGet();
     }
+    finally {
+        hideSpinner();
+    }
 });
 
 async function getData(city) {
@@ -62,28 +80,35 @@ async function getData(city) {
     return await promise.json();
 }
 
-liveLocationButton.addEventListener("click", () => {
-    const result = navigator.geolocation.getCurrentPosition(gotLocation, handleError);
-    city.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
-    localTime.innerText = result.location.localtime;
-    temp.innerText = `${result.current.temp_c}C`;
+getLocation.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(gotLocation, handleError);
 });
 
 async function getCurrData(lat, long) {
-    try {
-        const promise = await fetch(`${baseUrl}?key=${apiKey}&q=${lat},${long}&aqi=yes`);
-        return await promise.json();
-    }
-    catch(error) {
-        failedToGet();
-    }
+    const promise = await fetch(`${baseUrl}?key=${apiKey}&q=${lat},${long}&aqi=yes`);
+    return await promise.json();
 }
 
 async function gotLocation(position) {
-    const result = await getCurrData(position.coords.latitude, position.coords.longitude);
-    console.log(result);
-}
-
-function handleError() {
-    console.log("There is some issue");
+    showSpinner();
+    try {
+        const result = await getCurrData(position.coords.latitude, position.coords.longitude);
+        weatherIcon.style.display = "block";
+        city.innerHTML = `${result.location.name}, ${result.location.country}`;
+        localTime.innerText = result.location.localtime;
+        temp.innerText = `${result.current.temp_c}°C`;
+        const isDay = result.current.is_day;
+        if(isDay == 1) {
+            weatherIcon.src = `${result.forecast.forecastday[0].day.condition.icon}`;
+        }
+        else {
+            weatherIcon.src = "night_icon2.png";
+        }
+    } 
+    catch (error) {
+        failedToGet();   
+    }
+    finally {
+        hideSpinner();
+    }
 }
